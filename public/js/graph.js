ba7b9e5c0e80e1c1af5fff831ace03a66712be4f
bharrison3912 <bharrison@onshape.com>
2015-06-30 15:41:43 -0400

    c24

--- public/js/graph.js
+++ public/js/graph.js
@@ -317,7 +317,8 @@ function onGenerate2() {
           "&outputHeight=75&outputWidth=75&pixelSize=" + realSize / 75 +
       "&viewMatrix1=" + 0.707 + "&viewMatrix2=" + 0.707 + "&viewMatrix3=" + 0 + "&viewMatrix4=" + (-tX) +
       "&viewMatrix5=" + (-0.409) + "&viewMatrix6=" + 0.409 + "&viewMatrix7=" + 0.816 + "&viewMatrix8=" + (-tY) +
-      "&viewMatrix9=" + 0.577 + "&viewMatrix10=" + (-0.577) + "&viewMatrix11=" + 0.577 + "&viewMatrix12=" + (-tZ);
+      "&viewMatrix9=" + 0.577 + "&viewMatrix10=" + (-0.577) + "&viewMatrix11=" + 0.577 + "&viewMatrix12=" + (-tZ)
+      "&partId=0";
 
   $.ajax('/api/shadedView'+ options, {
     dataType: 'json',
